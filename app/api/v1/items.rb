10d34c563b3f0a912d3f6c9d826703dde0b51caf
Tomwey <tomwey@163.com>
2015-10-22 00:04:03 +0800

    do create item api

--- app/api/v1/items.rb
+++ app/api/v1/items.rb
@@ -2,7 +2,9 @@ module V1
   class Items < Grape::API
     
     resource :items do
-      desc "发布一个产品"
+      desc "发布一个产品" do
+        detail "注意：此接口还必须传入多张图片，参数的名字以：photo为前缀，后面跟数字，例如：photo0, photo1, photo2..."
+      end
       params do
         requires :title,    type: String,  desc: "产品标题"
         requires :tag_id,   type: Integer, desc: "类别id"
@@ -11,12 +13,25 @@ module V1
         requires :deposit,  type: Integer, desc: "产品保证金，不带单位，整数，例如：1000"
         requires :location, type: String,  desc: "发布产品的位置坐标，值格式为：(经度,纬度)用英文逗号分隔，例如：120.3454,20.3045"
         requires :placement,type: String,  desc: "发布产品所在位置逆向编码的建筑物名称, 比如小区或学校的名字，例如：绿地世纪城"
-        
+        optional :photo0,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo1,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo2,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo3,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo4,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo5,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
+        optional :photo6,   type: Rack::Multipart::UploadedFile, desc: "产品图片"
         optional :intro,    type: String,  desc: "产品新旧程度描述，例如：95成新"
         optional :note,     type: String,  desc: "备注"
       end
       post :create do
-        authenticate!
+        user = authenticate!
+        
+        tag = Tag.find_by(id: params[:tag_id])
+        if tag.blank?
+          return render_error(4004, '不正确的类别ID')
+        end
+        
+        item = Item.new(title: params[:title], tag_id: tag.id, fee: params[:fee], deposit: params[:deposit])
         
       end # end create
       

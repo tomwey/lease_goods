fd260f91a0a92ee66628b81a3f6570fc4b087a2d
Tomwey <tomwey@163.com>
2015-10-21 21:28:45 +0800

    update port config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ role :web, %w{deployer@120.132.57.133}
 role :db,  %w{deployer@120.132.57.133}
 
 set :server_name, "120.132.57.133"
-set :port, "9191"
+set :port, "9999"
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 # set :server_name, "www.shuiguoshe.com shuiguoshe.com"

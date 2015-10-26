d5e5fd4c7eed7180947126829c8d29ad24e34cf3
Tomwey <tomwey@163.com>
2015-10-26 22:40:58 +0800

    update deployment configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ role :web, %w{deployer@120.132.57.133}
 role :db,  %w{deployer@120.132.57.133}
 
 set :server_name, "lease-goods-origin.afterwind.cn"
-set :port, "80" # 默认都是80端口
+# set :port, "80" # 默认都是80端口
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 # set :server_name, "www.shuiguoshe.com shuiguoshe.com"
